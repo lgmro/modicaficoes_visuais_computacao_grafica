2075fb7a5f55585d140017a06a36d98bb8433319
lgmro <marinholgabriel@gmail.com>
2023-11-28 22:20:05 -0300

    Rollback

--- presenter/homeLayout.py
+++ presenter/homeLayout.py
@@ -150,7 +150,7 @@ class HomeLayout:
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
-    def change_scaling_event(self, new_scaling: float):
+    def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
 
