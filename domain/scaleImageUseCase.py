e67b7379abbcc134ca574f5036e2e236309bb709
lgmro <marinholgabriel@gmail.com>
2023-11-29 17:31:25 -0300

    Adjusting logger and name of input dialog

--- domain/scaleImageUseCase.py
+++ domain/scaleImageUseCase.py
@@ -8,11 +8,14 @@ class ScaleImageUseCase:
     def execute(self, image, value):
         original_image = image
         width, height = image.size
-        print(f'O tamanho original da imagem é: {width}x{height}')
+        self.logger.info(f'The original size of image is: Width: {width} x Height: {height}')
+
         scale_value = value / 100
         new_size = (int(width * scale_value), int(height * scale_value))
         scaled_image = original_image.resize(new_size, Image.LANCZOS)
         width, height = scaled_image.size
-        print(f'O tamanho da imagem após a mudança de escala é: {width}x{height}')
-        print(f'Redimensionado para: {value}%')
+
+        self.logger.info(f'The size of image after scale is: Width: {width} x Height: {height}')
+        self.logger.info(f'Resized to: {value}%')
+
         return scaled_image
